aa9cc3987b631c2b1badaaf26f7bd56c0de2d641
ksenya31 <kseniakalinnikova268@gmail.com>
2022-09-29 09:04:04 +0300

    added badges

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+[![Build status](https://ci.appveyor.com/api/projects/status/9797p5veh1am3ey4?svg=true)](https://ci.appveyor.com/project/Ksenya31/homework4)
--- src/test/java/ru/netology/domain/CardDeliveryTest.java
+++ src/test/java/ru/netology/domain/CardDeliveryTest.java
@@ -16,13 +16,13 @@ public class CardDeliveryTest {
     @Test
     public void shouldSuccessfulFormSubmission() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Анадырь");
+        $("[data-test-id=city] input").setValue("Самара");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
         String verificationDate = LocalDate.now() .plusDays(3)         //Текущая дата плюс 3 дня
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
         $("[data-test-id=date] input").setValue(verificationDate);
-        $("[data-test-id=name] input").setValue("Виктор Иванов");
-        $("[data-test-id=phone] input").setValue("+79206116499");
+        $("[data-test-id=name] input").setValue("Ксения Калинникова");
+        $("[data-test-id=phone] input").setValue("+79377773786");
         $("[data-test-id=agreement]").click();
         $x("//*[text()=\"Забронировать\"]").click();
         $("[data-test-id=notification]")
@@ -32,15 +32,15 @@ public class CardDeliveryTest {
     @Test
     public void shouldSuccessfulFormSubmissionAfterInteractingWithComplexElements() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Ан");
-        $(Selectors.byText("Анадырь")).click();
+        $("[data-test-id=city] input").setValue("Мо");
+        $(Selectors.byText("Москва")).click();
         $("[data-test-id=date] input").click();
-        int days = 4;                                                   //количество дней после даты по умолчанию
+        int days = 4;                    //количество дней после даты по умолчанию
         for ( int cycle = 0; cycle < days; cycle++) {
             $(".calendar").sendKeys(Keys.ARROW_RIGHT);
         }
         $(".calendar").sendKeys(Keys.ENTER);
-        $("[data-test-id=name] input").setValue("Виктор Иванов");
+        $("[data-test-id=name] input").setValue("Светлана Иванова");
         $("[data-test-id=phone] input").setValue("+79206116499");
         $("[data-test-id=agreement]").click();
         $(".button").shouldHave(Condition.text("Забронировать")).click();
